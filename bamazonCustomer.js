196298fba9bfcd3de15ed9d7636c00ac76b2e490
Ben Goligowski <51453763+btgoligowski24@users.noreply.github.com>
2019-09-28 08:33:33 -0500

    added table feature for logging out sql responses

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,15 +1,15 @@
 require("dotenv").config();
-let mysql = require("mysql");
-let credentials = require("./credentials.js");
-let inquirer = require("inquirer");
-
-let connection = mysql.createConnection(credentials.mySQL);
+const mysql = require("mysql");
+const credentials = require("./credentials.js");
+const inquirer = require("inquirer");
+const cTable = require("console.table");
+const connection = mysql.createConnection(credentials.mySQL);
 
 connection.connect(error => {
     if (error) throw error;
 });
 
 connection.query("SELECT * FROM products", (error, data) => {
-    console.log(data);
+    console.table("Products",data);
     connection.end();
 });
